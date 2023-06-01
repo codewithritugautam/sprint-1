71e234edff4a5d88d95e5c5c4638ccc76e7e6c7b
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-06-01 14:38:52 +0530

    Add files via upload

---
+++ AngularWSB2/AngularWSB2/src/test/java/springboot/batch2pm/awsb/emp/AngularWSB2/AngularWsb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.batch2pm.awsb.emp.AngularWSB2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularWsb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
