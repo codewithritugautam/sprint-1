a20afde870deced5cfed6b0ccd0a03a1db41f348
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-06-02 22:14:53 +0530

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,26 @@
+package JUnitExample2;
+
+public class MainClass {
+	public int add(int a,int b) {
+		return a+b;
+		}
+		public boolean MinMax(int a,int b) {
+		if(a>b)return true;
+		else return false;
+		}
+		public int displayNum(int i,int j,int k) {
+		if(i>j)return i;
+		else if(j>k)return j;
+		else return k;
+		}
+		public int oddeven(int i) {
+		if(i%2==0) {return 1;}
+		else return 0;
+		}
+		public long display(int i) {
+		for(;i<5;i++);
+		return i;
+		}
+		}
+
+
